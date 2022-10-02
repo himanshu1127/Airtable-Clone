70fca2882b34caf4123ed9de606bc3f7378b05fe
himanshu1127 <himanshug146.hg@gmail.com>
2022-10-02 18:35:34 +0530

    day2

--- home.js
+++ home.js
@@ -1,8 +1,47 @@
-let logoname = document.querySelector(".Brandname")
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 0) {
-        logoname.classList.add("scrolled")
-    } else {
-        logoname.classList.remove("scrolled")
-    }
-});
+
+
+
+
+document.querySelector(".product-button a").addEventListener("click", dispro)
+document.querySelector(".solution-button a").addEventListener("click", dissol)
+document.querySelector(".enterprise-button a").addEventListener("click", disent)
+document.querySelector(".resourses-button a").addEventListener("click", disres)
+
+let display = document.querySelector(".body");
+
+function dispro() {
+    display.classList.toggle("boom")
+    document.querySelector(".products").classList.add("boom2")
+
+}
+function dissol() {
+    display.classList.toggle("boom")
+    document.querySelector(".solutions").classList.add("boom2")
+}
+
+
+function disent() {
+    display.classList.toggle("boom")
+    document.querySelector(".Enterprise").classList.add("boom2")
+}
+
+
+function disres() {
+    display.classList.toggle("boom")
+    document.querySelector(".Resources").classList.add("boom2")
+}
+
+
+
+
+
+
+
+
+function myfun() {
+    display.classList.toggle("boom")
+    document.querySelector(".solutions").classList.add("boom2")
+}
+
+
+
